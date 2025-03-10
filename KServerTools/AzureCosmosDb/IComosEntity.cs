acf4df61abced92e0402f5e06d71aa41a2c3e768
Justin Dellamore <jdellamore@outlook.com>
2025-03-09 21:12:30 -0700

    missed change for reps

--- KServerTools/AzureCosmosDb/IComosEntity.cs
+++ KServerTools/AzureCosmosDb/IComosEntity.cs
@@ -4,3 +4,8 @@ public interface IComosEntity: IEntity {
     string Id { get; }
     string PartitionKey { get; }
 }
+
+public interface IComosEntityLookup : IEntityLookup {
+    string Id { get; }
+    string PartitionKey { get; }
+}
